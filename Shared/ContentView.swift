63e5c8256b9b0f3e8b2bc9362a7ffa714441ee28
hansen-ri <gridironhero@gmail.com>
2022-01-11 10:10:43 -0700

    Practice

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,10 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, World!")
             .padding()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
