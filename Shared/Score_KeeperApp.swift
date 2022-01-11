8ddaac7c202eab11dfc98b361717a8a806718a13
hansen-ri <gridironhero@gmail.com>
2022-01-11 08:28:53 -0700

    Initial Commit

---
+++ Shared/Score_KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Score_KeeperApp.swift
+//  Shared
+//
+//  Created by Riley Hansen on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Score_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
